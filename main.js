0f98e0b666617a31538831a751b153033b41eb0f
Neiyher Lopera <neiyher@hotmail.com>
2023-04-18 09:13:41 -0500

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -33,3 +34,76 @@ function toggleCarritoAside() {
     }
     aside.classList.toggle('inactive')
 }
+
+const productList = [];
+
+productList.push ({
+    name: 'Bici acrobática',
+    price: 1000000,
+    image: './Imagenes/Bici_acrobática_artwork.png',
+})
+productList.push ({
+    name: 'Bici carrera',
+    price: 1000000,
+    image: './Imagenes/Bici_carrera_artwork.png',
+})
+productList.push ({
+    name: 'Pokéball',
+    price: 200,
+    image: "./Imagenes/pokeball.jfif",
+})
+productList.push ({
+    name: 'Bici acrobática',
+    price: 1000000,
+    image: './Imagenes/Bici_acrobática_artwork.png',
+})
+productList.push ({
+    name: 'Bici carrera',
+    price: 1000000,
+    image: './Imagenes/Bici_carrera_artwork.png',
+})
+productList.push ({
+    name: 'Pokéball',
+    price: 200,
+    image: "./Imagenes/pokeball.jfif",
+})
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productInfoFigure.appendChild(productImgCart);
+        
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
